f4c43c967b78a184fcd9261b8eed2a5786f95a68
GitHubDoug <dubhglas.cambeuil@gmail.com>
2022-11-17 16:22:11 -0400

    ShinyTextCh2

--- ShinyTextCh2/ShinyTextCh2/app.R
+++ ShinyTextCh2/ShinyTextCh2/app.R
@@ -2,10 +2,28 @@
 library(shiny)
 
 ui <- fluidPage(
-  plotOutput("plot", width = "400px")
+  fluidRow(
+    column(3, 
+           numericInput("lambda1", label = "lambda1", value = 3),
+           numericInput("lambda2", label = "lambda2", value = 5),
+           numericInput("n", label = "n", value = 1e4, min = 0),
+           actionButton("simulate", "Simulate!")
+    ),
+    column(9, plotOutput("hist"))
+  )
 )
+
 server <- function(input, output, session) {
-  output$plot <- renderPlot(plot(1:5), res = 96)
+  x1 <- eventReactive(input$simulate, {
+    rpois(input$n, input$lambda1)
+  })
+  x2 <- eventReactive(input$simulate, {
+    rpois(input$n, input$lambda2)
+  })
+  
+  output$hist <- renderPlot({
+    freqpoly(x1(), x2(), binwidth = 1, xlim = c(0, 40))
+  }, res = 96)
 }
 
 # Run the application 
