a6c56aaa07a196fe3fc5f6bf87167d6e8c732cb1
GitHubDoug <dubhglas.cambeuil@gmail.com>
2022-11-24 13:48:41 -0400

    Simple_App.R skeleton

--- MultiCulti/Simple_app.R
+++ MultiCulti/Simple_app.R
@@ -11,9 +11,9 @@ ui <- fluidPage(
   titlePanel("XY Data Exploration"),
   varSelectInput("XVar", label = "Select Variable for X Axis", Data),
   varSelectInput("YVar", label = "Select Variable for Y Axis", Data),
-  #selectInput("ColourVar", label = "Select Variable for Symbol Colour", choices = c("None", colnames(Data)), selected = "Null"),
-  #selectInput("RowVar", label =  "Select Variable for Facet Row", choices = c("None", colnames(Data)), selected = "Null"),
-  #selectInput("ColVar", label = "Select Variable for Facet Column", choices = c("None", colnames(Data)), selected = "Null"),
+  varSelectInput("ColourVar", label = "Select Variable for Symbol Colour", Data),
+  selectInput("RowVar", label =  "Select Variable for Facet Row", choices = c("None", colnames(Data))),
+  #selectInput("ColVar", label = "Select Variable for Facet Column", Data),
   
   plotOutput("plot", width = "400px")
 )
@@ -21,10 +21,18 @@ ui <- fluidPage(
 #Specify the behaviour of the app by defining a server function.
 
 server <- function(input, output, session) {
+  if(reactive(input$RowVar) == "None"){
   output$plot <- renderPlot({
     ggplot(Data) + 
       geom_point(aes(x = !!input$XVar, y = !!input$YVar))
-    }, res = 96)
+  }, res = 96)
+  }
+  else{
+    output$plot <- renderPlot({
+    ggplot(Data) + 
+      geom_point(aes(x = !!input$XVar, y = !!input$YVar))
+  }, res = 96)
+}
 }
 
 #Execute shinyApp(ui, server) to construct and start a Shiny application from UI and server.
